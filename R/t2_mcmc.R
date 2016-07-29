955baf40920a0841f2495e8736002ee878d3887d
Fonti Kar <fonti.kar@gmail.com>
2016-07-29 12:12:49 +1000

    Code for running three chains for MCMC probability models added

--- R/t2_mcmc.R
+++ R/t2_mcmc.R
@@ -13,23 +13,39 @@ assocdat$Date <-as.Date(assocdat$Date, format="%m/%d/%Y")
 assocdat$Time <- as.factor(assocdat$Time)
 assocdat$Correct <- as.factor(assocdat$Correct)
 
+#################################################################################################################################################
+
 prior.test<- list(R = list(V =1,fix=1, nu = 0.002), G = list(G1 = list(V = diag(2), nu = 0.002)))
 
+chains <- c(runif(3, 0, 100))
 
-probcor.1<-MCMCglmm(Correct ~ Treatment*Trial+Batch, random = ~us(1+Trial):LizardID, family = "categorical", nitt = 2000000, thin = 1000, prior=prior.test, burnin = 15000, data=assocdat, verbose= T)
+probcor.1 <- list()
+  for(i in 1:3){
+    set.seed(chains[i])
+    probcor.1[[i]] <- MCMCglmm(Correct ~ Treatment*Trial+Batch, random = ~us(1+Trial):LizardID, family = "categorical", nitt = 150000, thin = 100, prior=prior.test, burnin = 15000, data=assocdat, verbose= T)
+  }
 
 saveRDS(probcor.1, file="output/t2_probcormod.1")
 
+#################################################################################################################################################
 
-correctonly.1<-MCMCglmm(Choose.only.correct.dish ~ Treatment*Trial+Batch, random = ~us(1+Trial):LizardID, family = "categorical", nitt = 2000000, thin = 1000, prior=prior.test, burnin = 15000, data=assocdat, verbose= T)
-
+correctonly.1 <- list()
+for(i in 1:3){
+  set.seed(chains[i])
+  correctonly.1[[i]] <- MCMCglmm(Choose.only.correct.dish ~ Treatment*Trial+Batch, random = ~us(1+Trial):LizardID, family = "categorical", nitt = 2000000, thin = 1000, prior=prior.test, burnin = 15000, data=assocdat, verbose= T)
+}
 
 saveRDS(correctonly.1, file="output/t2_correctonly.1")
 
+#################################################################################################################################################
+
 #Interaction Removed
 
-correctonly.1.inrm<-MCMCglmm(Choose.only.correct.dish ~ Treatment+Trial+Batch, random = ~us(1+Trial):LizardID, family = "categorical", nitt = 2000000, thin = 1000, prior=prior.test, burnin = 15000, data=assocdat, verbose= T)
-
+correctonly.1.inrm <- list()
+  for(i in 1:3){
+    set.seed(chains[i])
+    correctonly.1.inrm <- MCMCglmm(Choose.only.correct.dish ~ Treatment+Trial+Batch, random = ~us(1+Trial):LizardID, family = "categorical", nitt = 2000000, thin = 1000, prior=prior.test, burnin = 15000, data=assocdat, verbose= T)
+}
 
 saveRDS(correctonly.1.inrm, file="output/t2_correctonly.2")
 
